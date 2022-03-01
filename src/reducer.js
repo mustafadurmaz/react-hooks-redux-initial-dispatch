04e5010f7defeeafd12334e2137a8f17d511fb26
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-03-01 09:09:54 +0300

    Done

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,16 @@
-let state = { count: 0 };
 
-function changeState(state, action) {
+
+let state;
+function changeState(state={count:0}, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+
     default:
       return state;
   }
 }
+
 
 function dispatch(action) {
   state = changeState(state, action);
@@ -18,3 +21,6 @@ function render() {
   const app = document.querySelector("#app");
   app.textContent = state.count;
 }
+
+dispatch({ type: '@@INIT' });
+dispatch({ type: "counter/increment" });
